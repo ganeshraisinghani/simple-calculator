cb5a3ec74294089b19f8bfd1d94ce01b0fbdc5e2
ganesh <ganeshmohan8297@gmail.com>
2025-05-26 12:49:49 +0530

    Fourth Commit Modified!

--- test_calculator.py
+++ test_calculator.py
@@ -1,22 +1,27 @@
 import unittest
 from calculator import add, subtract, multiply, divide
 
+
 class TestCalculator(unittest.TestCase):
+
 
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
 
+
     def test_subtract(self):
         self.assertEqual(subtract(5, 2), 3)
         self.assertEqual(subtract(1, 5), -4)
         self.assertEqual(subtract(0, 0), 0)
 
+
     def test_multiply(self):
         self.assertEqual(multiply(2, 3), 6)
         self.assertEqual(multiply(-1, 5), -5)
         self.assertEqual(multiply(0, 10), 0)
+
 
     def test_divide(self):
         self.assertEqual(divide(6, 3), 2)
@@ -24,5 +29,7 @@ class TestCalculator(unittest.TestCase):
         with self.assertRaises(ValueError):
             divide(5, 0)
 
+
 if __name__ == '__main__':
     unittest.main()
+    
